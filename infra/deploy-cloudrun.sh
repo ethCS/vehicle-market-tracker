57b772a99cb3d3aced8ef8e0136588aab057d69d
ethCS <syrup199@gmail.com>
2026-05-04 21:16:39 -0600

    fix dockerfile

--- infra/deploy-cloudrun.sh
+++ infra/deploy-cloudrun.sh
@@ -8,6 +8,7 @@ gcloud run deploy $SERVICE \
   --image gcr.io/$PROJECT/$SERVICE \
   --platform managed \
   --region $REGION \
+  --clear-base-image \
   --allow-unauthenticated \
   --set-env-vars GCLOUD_PROJECT=$PROJECT,PUBSUB_TOPIC_INGEST=vehicle-ingest,PUBSUB_TOPIC_PRICES=price-ingest,FB_ADMIN_PROJECT_ID=$PROJECT,MARKETCHECK_COUNTRY=us,MARKETCHECK_ROWS=25 \
   --set-secrets MARKETCHECK_API_KEY=MARKETCHECK_API_KEY:latest,MARKETCHECK_CLIENT_SECRET=MARKETCHECK_CLIENT_SECRET:latest,INGEST_SECRET=INGEST_SECRET:latest \
